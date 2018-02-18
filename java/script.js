84f93510d8dac861a2ddf6914d7185535843274a
dhiraj <dhirajwagh90@gmail.com>
2018-02-18 13:44:02 +0530

    init

---
+++ java/script.js
@@ -0,0 +1,18 @@
+ function myfunction() {
+     alert("Welcome spark ");
+ }
+ var a, b;
+
+ function addition(a, b) {
+     var c = a + b;
+     return c;
+ }
+ var x = addition(5, 6);
+ console.log(x);
+
+ function subtraction(a, b) {
+     var c = a - b;
+     return c;
+ }
+ var y = subtraction(5, 6);
+ console.log(y);
